fe160b88649cc938c1d10d0f73b8d3e5a378c578
Elia Manara <elia.manara@aiknow.io>
2020-10-17 18:39:37 +0200

    Added form and controller

--- src/com/luv2code/springdemo/mvc/HomeController.java
+++ src/com/luv2code/springdemo/mvc/HomeController.java
@@ -1,7 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
-        import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
