78f683172ee9309fde4c30f33bbf04b4c648dd17
Elia Manara <elia.manara@aiknow.io>
2020-10-31 17:06:04 +0100

    @RequestParam

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -19,12 +20,26 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    /*
     // New controller method to read form data and add data to the model
     @RequestMapping("/processFormVersionTwo")
     public String letsShoutDude(HttpServletRequest request, Model model) {
 
         // Read the request parameter from the HTML form
         String theName = request.getParameter("studentName");
+        // Convert the data to uppercase
+        theName = theName.toUpperCase();
+        // Create the message
+        String result = "Yo!" + theName;
+        // Add the message to the model
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+    */
+
+    // New controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
         // Convert the data to uppercase
         theName = theName.toUpperCase();
         // Create the message
