7a708ee53f9c4d10a90530460489d67cb5a385b2
Elia Manara <elia.manara@aiknow.io>
2020-11-06 09:26:15 +0100

    Country options 1

--- out/artifacts/spring_mvc_demo_war_exploded/WEB-INF/view/student-confirmation.jsp
+++ out/artifacts/spring_mvc_demo_war_exploded/WEB-INF/view/student-confirmation.jsp
@@ -12,6 +12,10 @@
 
     The student is confirmed: ${student.firstName}  ${student.lastName}.
 
+    <br><br>
+
+    Student's country: ${student.country}.
+
 </body>
 
 </html>
--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -1,12 +1,24 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
 
+    private LinkedHashMap<String, String> countryOptions;
+
     public Student() {
+
+        // Populate country options: used ISO country code
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
     }
 
     public String getFirstName() {
