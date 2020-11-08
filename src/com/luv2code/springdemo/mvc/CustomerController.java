7e78c4c35d8167bdff5613375f5d69ca52fb3d44
Elia Manara <elia.manara@aiknow.io>
2020-11-08 09:22:46 +0100

    Inserted initBinder

--- src/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,14 +1,28 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // Add an initBinder to convert trim input strings
+    // Remove leading adn trailing whitespaces
+    // Resolve issue for validation (whitespaces passing validation)
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model theModel) {
@@ -21,6 +35,9 @@ public class CustomerController {
     @RequestMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
                               BindingResult theBindingResult) {
+
+        // Trick to check what's passed, even whitespaces
+        System.out.println("Last name: |" + theBindingResult.getNestedPath() + "|");
 
         if (theBindingResult.hasErrors())
             return "customer-form";
